89b5e2e4d6273474790f1a1f984ffda213c14823
Sean <seanom12@gmail.com>
2022-10-27 18:52:41 -0500

    add buttons

---
+++ calculator.js
@@ -0,0 +1,20 @@
+function add(){
+    let total = 0;
+    for (let i = 0; i < arguments.length; i++) {
+        total += arguments[i];
+        return total;
+    }
+}
+console.log(add(1, 2, 3, 4 ,5));
+
+function subtract(){
+
+}
+
+function multiply(){
+
+}
+
+function divide(){
+
+}
