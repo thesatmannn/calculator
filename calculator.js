9f85bea10454ea1875901ad992596554f4c4b640
Sean <seanom12@gmail.com>
2022-11-03 20:00:27 -0500

    add operate function

--- calculator.js
+++ calculator.js
@@ -1,1 +1,34 @@
-function maths(x, y, op){
+const buttons = document.querySelectorAll('button');
+const display = document.getElementById('display');
+let displayValue = 0;
+
+function operate(x, y, operator){
+    if(operator == '+'){
+        return add();
+    }
+    else if (operator == '-'){
+        return subtract();
+    }
+    else if (operator == '*'){
+        return multiply();
+    }
+    else {
+        divide();
+    }
+}
+
+function add(x, y){
+    return x + y;
+}
+
+function subtract(x, y){
+    return 
+}
+
+function multiply(x, y){
+    return x * y;
+}
+
+function divide(x, y){
+    return x / y;
+}
